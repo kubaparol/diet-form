9e7329e48131dd5c899f4c02465d638c36a36048
Jakub Parol <kubciop33@gmail.com>
2022-06-18 17:04:35 +0200

    feat(Button): create Button component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import StyledForm from "./Form.styled";
 
+import Button from "../Button";
+
 const Form = () => {
-  return <StyledForm></StyledForm>;
+  return (
+    <StyledForm>
+      <Button>Dalej</Button>
+    </StyledForm>
+  );
 };
 
 export default Form;
